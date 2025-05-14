1e756f034cda3efc100c31d84aad4f039d78bb16
Ping <nuzer0707@gmail.com>
2025-05-14 15:40:49 +0800

    0517

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -87,6 +87,17 @@ public class BookController {
 	}
 	
 	@PatchMapping("/{id}")
+	public ResponseEntity<ApiResponse<Book>> updateBookNameAndPrice(@PathVariable Integer id ,@RequestBody Book book){
+		try {
+				bookService.updateBookNameAndPrice(id,book.getName(), book.getPrice());
+				return ResponseEntity.ok(ApiResponse.success("修改價格成功", book));
+		} catch (BookException e) {
+				return ResponseEntity.badRequest().body(ApiResponse.error(e.getMessage()));
+		}
+	}
+	
+	
+	@PatchMapping("/price/{id}")
 	public ResponseEntity<ApiResponse<Book>> updateBookPrice(@PathVariable Integer id ,@RequestBody Book book){
 		try {
 				bookService.updateBookPrice(id, book.getPrice());
--- src/main/java/com/example/demo/model/Book.java
+++ src/main/java/com/example/demo/model/Book.java
@@ -8,9 +8,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Book {
-	private Integer id;
-	private String name; 
-	private Double price;
-	private Integer amount;
-	private Boolean pub;
+	private Integer id; //id
+	private String name; //書名 
+	private Double price; //價格
+	private Integer amount; //數量
+	private Boolean pub; //出刊/停刊
 }
