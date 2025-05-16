592d0ba09e1fcef503f7b215937fe5a39afcffe6
Ping <nuzer0707@gmail.com>
2025-05-16 14:14:30 +0800

    0516

--- src/main/java/com/example/demo/controller/SSRBookController.java
+++ src/main/java/com/example/demo/controller/SSRBookController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.exception.BookException;
@@ -69,7 +70,7 @@ public class SSRBookController {
 		
 	}
 	
-	@PostMapping("/edit/{id}")
+	@PutMapping("/edit/{id}")
 	public String editBook(@PathVariable Integer id,Book book,Model model) {
 		
 		try {
